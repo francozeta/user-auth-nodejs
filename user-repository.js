de239c19d196c27577d5403ee82889b2154192ed
Franco Zeta <perufra@hotmail.com>
2024-07-27 15:39:44 -0500

    Add functions to make sure if the username doesn't exists with the method findOne

--- user-repository.js
+++ user-repository.js
@@ -1,5 +1,7 @@
 import DBLocal from 'db-local'
+import crypto from 'crypto'
 const { Schema } = new DBLocal({ path: './db' })
+
 const User = Schema('User', {
   _id: { type: String, required: true },
   username: { type: String, required: true },
@@ -14,8 +16,20 @@ export class UserRepository {
 
     if (typeof password !== 'string') throw new Error('password must be a string')
     if (password.length < 6) throw new Error('password must be at least 6 characters long')
-  }
-  static login ({ username, password }) {
 
+    // 2. make sure if the username doesn't exists
+    const user = User.findOne({ username })
+    if (user) throw new Error('Username already exists')
+
+    const id = crypto.randomUUID()
+    User.create({
+      _id: id,
+      username,
+      password
+    }).save()
+
+    return id
   }
+
+  static login ({ username, password }) { }
 }
